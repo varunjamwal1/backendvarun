29f8e39a49b1ee5c09116394d59e27bb03da19bb
varunjamwal1 <varunjamwal933@gmail.com>
2026-03-21 13:43:47 +0530

    fix: Mongoose pre-save hook calls next, CORS allows 5174, numeric order payload

--- models/User.js
+++ models/User.js
@@ -14,9 +14,9 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-  
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
